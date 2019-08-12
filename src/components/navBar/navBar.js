f71c640d2c9b1fb31dfabfc3a355b25dadaf5c98
Jose Alvaro Flores Gambarelli <Floresgambarelli@gmail.com>
2019-08-12 11:32:28 -0600

    adds navbar component

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './navBar.css';
 
-class navBar extends Component {
+class NavBar extends Component {
     render() {
         return (
             <div className="navBarContainer">
-                
+                <i class="fas fa-home"></i>
+                <i class="fas fa-bell"></i>
+                <i class="fas fa-user"></i>
             </div>
         );
     }
 }
 
-export default navBar;
+export default NavBar;
